c2844b1c1e2779dfe217cb5c4e5dd7a6933eeb0c
Benjamin Dalton <benjamin.dalton@theaa.com>
2024-07-20 20:57:30 +0100

    Added type hints

--- my_project/scr/metrics.py
+++ my_project/scr/metrics.py
@@ -1,3 +1,4 @@
+import numpy as np
 from abc import ABC, abstractmethod
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
@@ -8,25 +9,25 @@ class Metric(ABC):
 
 class AccuracyMetric(Metric):
 
-    def calculate(self, y, y_pred):
+    def calculate(self, y: np.ndarray, y_pred: np.ndarray):
         name = 'accuracy'
         accuracy = accuracy_score(y, y_pred)
         return name, accuracy
 
 class PrecisionMetric(Metric):
-    def calculate(self, y, y_pred):
+    def calculate(self, y: np.ndarray, y_pred: np.ndarray):
         name = 'precision'
         precision = precision_score(y, y_pred)
         return name, precision
 
 class RecallMetric(Metric):
-    def calculate(self, y, y_pred):
+    def calculate(self, y: np.ndarray, y_pred: np.ndarray):
         name = 'recall'
         recall = recall_score(y, y_pred)
         return name, recall
 
 class FScoreMetric(Metric):
-    def calculate(self, y, y_pred):
+    def calculate(self, y: np.ndarray, y_pred: np.ndarray):
         name = 'f_score'
         f1 = f1_score(y, y_pred)
         return name, f1
