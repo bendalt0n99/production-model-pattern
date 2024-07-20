cf2f7bf94377039ef2c5a9e5b5ac53f7068e38a7
Benjamin Dalton <benjamin.dalton@theaa.com>
2024-07-20 19:19:46 +0100

    Cleared main

--- my_project/main.py
+++ my_project/main.py
@@ -4,35 +4,16 @@ from my_project.scr.models import LightGBMModel
 from my_project.scr.model_runner import ModelRunner
 
 def main():
-    train_path = 'data/train.csv'
-    test_path = 'data/test.csv'
+    train_path = '/Users/821069/personal/data_science_project/showcase-data-science-project/data/train.csv'
+    test_path = '/Users/821069/personal/data_science_project/showcase-data-science-project/data/test.csv'
     train_loader = DataLoader(loader = PandasLoader(train_path))
     test_loader = DataLoader(loader = PandasLoader(test_path))
     metrics = [
         AccuracyMetric()
     ]
     params = None
-    target = 'price_range'
+    target = ''
     features = [
-        'battery_power',
-        'clock_speed',
-        'dual_sim',
-        'fc',
-        'four_g',
-        'int_memory',
-        'm_dep',
-        'mobile_wt',
-        'n_cores',
-        'pc',
-        'px_height',
-        'px_width',
-        'ram',
-        'sc_h',
-        'sc_w',
-        'talk_time',
-        'three_g',
-        'touch_screen',
-        'wifi'
     ]
     model = LightGBMModel(params = params)
 
